629896b3bf87ad5ac9b785b5b3649194b0f65666
VinsonAlter <vinson7768@gmail.com>
2024-07-05 20:35:32 +0700

    fix alert tailwind css

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import {Form, useActionData, useLocation, Navigate, redirect, useNavigate} from 'react-router-dom'
 import axios from 'axios'
 import { Alert } from 'flowbite-react'
-// import {Alert} from 'flowbite'
+import './index.css'
 
 export default function Login() {
   // let's try making login form via tailwind
@@ -52,6 +52,23 @@ export default function Login() {
             <Alert color="failure">
                 <span className="font-medium">Berhasil Logout!</span>
             </Alert>
+            // <div class="flex flex-col gap-2 p-4 text-sm border-red-500 bg-red-100 text-red-700 dark:bg-red-200 dark:text-red-800 rounded-lg" role="alert"><div class="flex items-center" data-testid="flowbite-alert-wrapper"><div><span class="font-medium">Berhasil Logout!</span></div></div></div>
+            
+//             <div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+//   <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+//     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+//   </svg>
+//   <span class="sr-only">Info</span>
+//   <div class="ms-3 text-sm font-medium">
+//     A simple info alert with an <a href="#" class="font-semibold underline hover:no-underline">example link</a>. Give it a click if you like.
+//   </div>
+//   <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-2" aria-label="Close">
+//     <span class="sr-only">Close</span>
+//     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+//       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+//     </svg>
+//   </button>
+// </div>
           ) : ''}
           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-screen">  
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -69,7 +86,9 @@ export default function Login() {
                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                     <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                   </div>
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign In</button>
+                  <button type="submit" className="w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                    Submit
+                  </button>
                 </Form>
               </div>
             </div>
